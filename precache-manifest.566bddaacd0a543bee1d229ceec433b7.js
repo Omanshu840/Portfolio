cafd7aef92fef2ab8ad590b531268a2cf9ebc057
Omanshu480 <omanshumahawar.181co237@nitk.edu.in>
2020-10-20 01:28:44 +0530

    Updates

---
+++ precache-manifest.566bddaacd0a543bee1d229ceec433b7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddbcc3cced33f34cceb38af9ea82333d",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "646935898be484c55c62",
+    "url": "/Portfolio/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "fdc05a7cce772c0aba77",
+    "url": "/Portfolio/static/css/main.5a82db21.chunk.css"
+  },
+  {
+    "revision": "646935898be484c55c62",
+    "url": "/Portfolio/static/js/2.7089a929.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/Portfolio/static/js/2.7089a929.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdc05a7cce772c0aba77",
+    "url": "/Portfolio/static/js/main.32ad49ab.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "56d6f1d3f777df56e432c10ca0786db7",
+    "url": "/Portfolio/static/media/activity1.56d6f1d3.png"
+  },
+  {
+    "revision": "4356845a80da855c47d284f3cfc1c053",
+    "url": "/Portfolio/static/media/activity2.4356845a.png"
+  },
+  {
+    "revision": "7b27900ab6972750ba380c8bd27950df",
+    "url": "/Portfolio/static/media/project1.7b27900a.png"
+  },
+  {
+    "revision": "e9d34dc0eccfbeb2c13bb736e8862851",
+    "url": "/Portfolio/static/media/project2.e9d34dc0.png"
+  },
+  {
+    "revision": "7ce61cacb0b9fc906b0bc93f74b8dd9e",
+    "url": "/Portfolio/static/media/project3.7ce61cac.png"
+  }
+]);
